dde7ff9278f0da59dba2cd9d85562f1d2c72a049
thestarkor <sk_and_mc@kaist.ac.kr>
2020-08-24 17:00:49 +0900

    argparser

--- main.py
+++ main.py
@@ -1,39 +1,56 @@
 from tensorflow.keras.optimizers import Adam, SGD
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, TensorBoard
+import argparse
 
 from data_generator import train_data_generator, test_data_generator
 from utils import mae, psnr
 from model import edsr
 
-DATA_DIR = "./"
-N_TRAIN_DATA = 14
-N_TEST_DATA = 5
-BATCH_SIZE = 3
 
-model = edsr(scale=4, num_filters=256, num_res_blocks=32, res_block_scaling=0.1)
+if __name__ == "__main__":    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("N_TRAIN_DATA", type=int)
+    parser.add_argument("N_TEST_DATA", type=int)
+    parser.add_argument("N_RES_BLOCK", type=int)
+    parser.add_argument("BATCH_SIZE", type=int)
+    parser.add_argument("EPOCHS", type=int)
+    args = parser.parse_args()
 
-model.summary(line_length=150)
+    DATA_DIR = "../src/"
+    FILE_PATH = "./models/srcnn_div2k.hdf5"
+    TRAIN_PATH = "DIV2K_train_HR"
+    TEST_PATH = "DIV2K_valid_HR"
 
-lr_decay = ReduceLROnPlateau(
-    monitor="loss", factor=0.5, patience=10, verbose=1, min_lr=1e-5
-)
-checkpointer = ModelCheckpoint("edsr.h5", verbose=1, save_best_only=True)
-callback_list = [lr_decay, checkpointer]
+    N_TRAIN_DATA = args.N_TRAIN_DATA
+    N_TEST_DATA = args.N_TEST_DATA
+    N_RES_BLOCK = args.N_RES_BLOCK
+    BATCH_SIZE = args.BATCH_SIZE
+    EPOCHS = args.EPOCHS
 
-optimizers = Adam(lr=1e-4, beta_1=0.9, beta_2=0.99)
+    train_data_generator = train_data_generator(DATA_DIR, TRAIN_PATH, scale=4.0, batch_size=2)
 
-model.compile(loss=mae, metrics=[psnr], optimizer=optimizers)
+    test_x, test_y = next(
+        test_data_generator(DATA_DIR, TEST_PATH, scale=4.0, batch_size=2, shuffle=False)
+    )
 
-train_data_generator = train_data_generator(DATA_DIR, "train", scale=4.0, batch_size=2)
+    model = edsr(scale=4, num_filters=256, num_res_blocks=N_RES_BLOCK, res_block_scaling=0.1)
 
-test_x, test_y = next(
-    test_data_generator(DATA_DIR, "test", scale=4.0, batch_size=2, shuffle=False)
-)
+    model.summary(line_length=150)
 
-model.fit_generator(
-    train_data_generator,
-    validation_data=(test_x, test_y),
-    steps_per_epoch=N_TRAIN_DATA // 2,
-    epochs=100,
-    callbacks=callback_list,
-)
+    lr_decay = ReduceLROnPlateau(
+        monitor="loss", factor=0.5, patience=10, verbose=1, min_lr=1e-5
+    )
+    checkpointer = ModelCheckpoint(FILE_PATH, verbose=1, save_best_only=True)
+    callback_list = [lr_decay, checkpointer]
+
+    optimizers = Adam(lr=1e-4, beta_1=0.9, beta_2=0.99)
+
+    model.compile(loss=mae, metrics=[psnr], optimizer=optimizers)
+
+    model.fit_generator(
+        train_data_generator,
+        validation_data=(test_x, test_y),
+        steps_per_epoch=N_TRAIN_DATA // BATCH_SIZE,
+        epochs=EPOCHS,
+        callbacks=callback_list,
+    )
